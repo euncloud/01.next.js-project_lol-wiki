205bb52cbe9836e00438d5be588ade8d845f441b
Eunyeong Park <eunyeong.cloud@gmail.com>
2024-12-19 11:36:23 +0900

    fix: 이미지 내부 속성값 버전에 맞게 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="pt-[60px] bg-black">
-      <div className="w-full h-[calc(100vh-60px)] flex items-center justify-center">
+      <div className="w-full h-[calc(100vh-60px)] flex items-center justify-center object-cover">
         <Image
           src={"/champion-teemo-astronaut-splash.jpg"}
-          layout="fill"
-          objectFit="contain"
-          objectPosition="top"
           alt="홈 이미지"
+          width={1920}
+          height={1080} 
+          className="w-full h-[100vh] object-cover top-0 absolute"
+          priority={false}
         />
         <div className="absolute top-[20%] left-[25%]">
           <Link href="/champions">
